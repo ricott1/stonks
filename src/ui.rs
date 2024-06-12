c0322ce024e07f449b9899ef2d58966f1ddc8039
Alessandro Ricottone <ricott2@gmail.com>
2024-06-12 15:31:02 +0200

    stuff

--- src/ui.rs
+++ src/ui.rs
@@ -68,6 +68,7 @@ impl<'a> Ui<'a> {
         let split = Layout::vertical([
             Constraint::Min(0),
             Constraint::Length(1),
+            Constraint::Length(6),
             Constraint::Length(1),
         ])
         .split(area);
